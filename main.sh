acf3cf12163481c9f0a5604bf40f757f7f9e407d
Shana Cheng <shanachengg@gmail.com>
2021-10-11 15:44:02 -0400

    Added a script to execute appropriate program according to user input

---
+++ main.sh
@@ -0,0 +1,7 @@
+echo Input 1 if you would like to encode a message 
+echo Input 2 if you would like to decode a message given a key
+echo Input 3 if you would like to decode a message with only the length of the key
+
+read -p "Option: " var
+
+python3 task$var.py
